c371fa8d5eff211e94165b9329957f5253bc7fd8
Infrae <info@infrae.com>
2002-12-18 21:26:23 +0000

    new feature: export error message texts to xml added some xml escapes where is seems to be necessary

--- FormToXML.py
+++ FormToXML.py
@@ -14,8 +14,8 @@ def formToXML(form):
     write('<form>\n')
     # export form settings
     # XXX Should we be encoding some text?
-    write('  <title>%s</title>\n' % form.title)
-    write('  <name>%s</name>\n' % form.name)
+    write('  <title>%s</title>\n' % escape(form.title))
+    write('  <name>%s</name>\n' % escape(form.name))
     write('  <action>%s</action>\n' % form.action)
     write('  <enctype>%s</enctype>\n' % form.enctype)
     write('  <method>%s</method>\n\n' % form.method)
@@ -23,7 +23,7 @@ def formToXML(form):
     write('  <groups>\n')
     for group in form.get_groups():
         write('    <group>\n')
-        write('      <title>%s</title>\n' % group)
+        write('      <title>%s</title>\n' % escape(group))
         write('      <fields>\n\n')
         for field in form.get_fields_in_group(group):
             write('      <field><id>%s</id> <type>%s</type>\n' % (field.id, field.meta_type))
@@ -33,6 +33,8 @@ def formToXML(form):
                     continue
                 if type(value) == type(1):
                     write('          <%s type="int">%s</%s>\n' % (key, escape(str(value)), key))
+                elif type(value) == type([]):
+                    write('          <%s type="list">%s</%s>\n' % (key, escape(str(value)), key))
                 else:
                     write('          <%s>%s</%s>\n' % (key, escape(str(value)), key))
             write('        </values>\n')
@@ -41,6 +43,11 @@ def formToXML(form):
                 if value:
                     write('          <%s>%s</%s>\n' % (key, escape(str(value._text)), key))
             write('        </tales>\n')
+            write('        <messages>\n')
+            for message_key in field.get_error_names():
+                write('          <message name="%s">%s</message>\n' %
+                      (escape(message_key), escape(field.get_error_message(message_key))))
+            write('        </messages>\n')                
             write('      </field>\n')
         write('      </fields>\n')
         write('    </group>\n')
