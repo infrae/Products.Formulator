cc503ea68fb9489728b8a1de68e6fac447a10723
Infrae <info@infrae.com>
2003-04-02 09:43:56 +0000

    add startup trigger for Zope2.6

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,8 +1,15 @@
 # Copyright (c) 2002 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.1 $
+# $Revision: 1.2 $
 import unittest
 import Zope
+
+try:
+    from Zope import startup
+    startup()
+except ImportError:
+    # startup is only in Zope2.6
+    pass
 
 from Products.Formulator.tests import test_Form, test_validators, test_serialize
 
