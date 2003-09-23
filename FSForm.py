b0d44472642a95cfd3a2d5fda30d9c02c7b6003b
Infrae <info@infrae.com>
2003-09-23 10:10:42 +0000

    undid changes for i18n_encoding accidentally made in HEAD

--- FSForm.py
+++ FSForm.py
@@ -32,31 +32,6 @@ class FSForm(FSObject, ZMIForm):
         return None
 
     def _readFile(self, reparse):
-        s = open(expandpath(self._filepath),"rb").read()
-	startenc = s.find('encoding="') + len('encoding="')
-	xmlencoding = s[startenc:s.find('"',startenc + 2)]
-	if xmlencoding.lower() == "utf-8":
-	    for e in ("utf-8","iso-8859-1"):
-		try:
-		    s = unicode(s,e).encode('utf-8') # is it realy utf-encoded
-		    break
-		except:
-		    pass
-	    
-        # update the form with the xml data
-        try:
-            XMLToForm(s, self)
-        except:
-            # bare except here, but I hope this is ok, as the
-            # exception should be reraised
-            # (except if the LOG raises another one ... should we be more paranoid here?)
-            import zLOG
-            zLOG.LOG('Formulator.FSForm',zLOG.ERROR,
-                     'error reading form from file '+expandpath(self._filepath))
-            raise
-        
-
-    def old_readFile(self, reparse):
         f = open(expandpath(self._filepath), 'rb')
         # update the form with the xml data
         try:
--- FormToXML.py
+++ FormToXML.py
@@ -1,93 +1,14 @@
 from StringIO import StringIO
 from cgi import escape
-import re
-import sys
-
-def getEncoding(self):
-    "return the encoding"
-    charset_re=re.compile(r'text/[0-9a-z]+\s*;\s*' +
-	                  r'charset=([-_0-9a-z]+' +
-			  r')(?:(?:\s*;)|\Z)',
-			  re.IGNORECASE)
-    # Encode the Unicode data as requested
-    response = self.REQUEST.get("RESPONSE")    
-    if response.headers.has_key('content-type'):
-        match = charset_re.match(response.headers['content-type'])
-        if match:
-            return match.group(1)
-    # Use the default character encoding
-    return 'iso-8859-15'
-
-def neuformToXML(form, prologue=1):
-    """Takes a formulator form and serializes it to an XML representation.
-    """
-    f = StringIO()
-    write = f.write
-    #encoding = sys.getdefaultencoding()
-    encoding = "iso-8859-1"
-    if prologue:
-        write(u'<?xml version="1.0" encoding="%(encoding)s"?>\n\n' % vars())
-    write(u'<form>\n')
-    # export form settings
-    # XXX Should we be encoding some text?
-    write(u'  <title>%s</title>\n' % escape(form.title))
-    write(u'  <name>%s</name>\n' % escape(form.name))
-    write(u'  <action>%s</action>\n' % form.action)
-    write(u'  <enctype>%s</enctype>\n' % form.enctype)
-    write(u'  <method>%s</method>\n\n' % form.method)
-    # export form groups
-    write(u'  <groups>\n')
-    for group in form.get_groups():
-        write(u'    <group>\n')
-        write(u'      <title>%s</title>\n' % escape(group))
-        write(u'      <fields>\n\n')
-        for field in form.get_fields_in_group(group):
-            write(u'      <field><id>%s</id> <type>%s</type>\n' % (field.id, field.meta_type))
-            write(u'        <values>\n')
-            items = field.values.items()
-            items.sort()
-            for key, value in items:
-                if value is None:
-                    continue
-                if type(value) == type(1.1):
-                    write(u'          <%s type="float">%s</%s>\n' % (key, escape(str(value)), key))
-                if type(value) == type(1):
-                    write(u'          <%s type="int">%s</%s>\n' % (key, escape(str(value)), key))
-                elif type(value) == type([]):
-                    write(u'          <%s type="list">%s</%s>\n' % (key, escape(str(value)), key))
-                else:
-                    write(u'          <%s>%s</%s>\n' % (key, escape(str(value)), key))
-            write(u'        </values>\n')
-
-            write(u'        <tales>\n')
-            items = field.tales.items()
-            items.sort()
-            for key, value in items:
-                if value:
-                    write(u'          <%s>%s</%s>\n' % (key, escape(str(value._text)), key))
-            write(u'        </tales>\n')
-
-            write(u'        <messages>\n')
-            for message_key in field.get_error_names():
-                write(u'          <message name="%s">%s</message>\n' %
-                      (escape(message_key), escape(field.get_error_message(message_key))))
-            write(u'        </messages>\n')
-            write(u'      </field>\n')
-        write(u'      </fields>\n')
-        write(u'    </group>\n')
-    write(u'  </groups>\n')
-    write(u'</form>')
-
-    return f.getvalue()
 
 def formToXML(form, prologue=1):
     """Takes a formulator form and serializes it to an XML representation.
     """
     f = StringIO()
     write = f.write
-    encoding = getEncoding(form)
+
     if prologue:
-        write('<?xml version="1.0" encoding="%(encoding)s"?>\n\n' % vars())
+        write('<?xml version="1.0" encoding="iso-8859-1"?>\n\n')
     write('<form>\n')
     # export form settings
     # XXX Should we be encoding some text?
