dc2a22a046d207cc76159b6776fa37b182931e89
Infrae <info@infrae.com>
2004-04-29 17:00:57 +0000

    updated Copyright date in product code

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,6 +1,6 @@
-# Copyright (c) 2002 Infrae. All rights reserved.
+# Copyright (c) 2002-2004 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.2 $
+# $Revision: 1.3 $
 import unittest
 import Zope
 
