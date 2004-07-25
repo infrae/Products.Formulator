9b428e1b5584597924963ee3364da48048abe156
Infrae <info@infrae.com>
2004-07-25 00:22:38 +0000

    activated the "test list items" test -- I found that the problems on the validation side have been fixed, and went to fix the parts on the field rendering side.  while doing so added the actually missing validation tests, dropped dead or unnecessary code and let the test run twice; one time for single items and one for miltiple item selections.

--- tests/test_Form.py
+++ tests/test_Form.py
@@ -47,7 +47,13 @@ class FormTestCase(unittest.TestCase):
         """
         self.failIf(self.form.has_field('title'))
 
-    def _test_list_values(self):
+    def test_list_items(self):
+        self._test_list_items('ListField')
+
+    def test_multi_list_items(self):
+        self._test_list_items('MultiListField')
+        
+    def _test_list_items(self, list_field_type):
         """ test if a list of values returned by TALES (override) expressions
         is interpreted properly.
         If a TALES tab returns a sequence of items and some item is
@@ -55,31 +61,17 @@ class FormTestCase(unittest.TestCase):
         has lead to a item text of 'o' and a display value of 'k'
         (as the this is actually a sequence of length 2 ...)
          See http://sourceforge.net/mailarchive/forum.php?thread_id=1359918&forum_id=1702
-
-        Actually the original problem still does not work,
-        as passing a list of int's is not yet supported.
-        If it should, please uncomment the second part of the test.
         """
 
-        # XXX deactivated: this maybe should not be fixed at all
-
-        self.form.manage_addField('list_field', 'Test List Field', 'ListField')
-
-        # adding a python script to be called by the override tab
-        # FIXME: the following does not work, as the fake-request
-        # does not have a "form" atribute (?)
-        #self.root.manage_addProduct['PythonScripts'] \
-        #         .manage_addPythonScript('override_test', 'Test for override')
-        #
-        #self.root._getOb('override_test').write("return ['ok', 'no']\n")
+        self.form.manage_addField('list_field', 'Test List Field', list_field_type)
 
         self.form.override_test = PythonScript('override_test')
         self.form.override_test.write("return ['ok', 'no']\n")
-        # ps._makeFunction()
-
 
         list_field = getattr(self.form, 'list_field')
         list_field.values['items'] = [ ('ok', 'ok'), ('no', 'no') ]
+
+        list_field.values['first_item'] = 'on'
 
         items1 = list_field.render()
 
@@ -87,24 +79,36 @@ class FormTestCase(unittest.TestCase):
         list_field.tales['items'] = TALESMethod("python:['ok', 'no']")
         items2 = list_field.render()
 
+        # test render
         self.assertEquals(items1, items2)
-
-        # test override
+        # test render_view
+        self.assertEquals('ok', list_field.render_view('ok') )
+        # test validation ... fake request with a dictionary ...
+        list_field.validate({'field_list_field':'ok'})
+        
+        # test override (most probably superfluous)
         del list_field.tales['items']
         list_field.overrides['items'] = Method('override_test')
         items2 = list_field.render()
 
         self.assertEquals(items1, items2)
 
-        # test if TALES returns a list of e.g. int
-        #list_field.values['items'] = [ ('42', '42'), ('88', '88') ]
-        #
-        #items1 = list_field.render()
-        #
-        #list_field.tales['items'] = TALESMethod("python:[42, 88]")
-        #items2 = list_field.render()
-        #
-        #self.assertEquals(items1, items2)
+        # missing: if returning a list of int,
+        # rendering does work here, but validation fails.
+        # maybe it should fail, but on the other hand this is a FAQ on the list ...
+        del list_field.overrides['items']
+        # test when TALES returns a list of e.g. int
+        list_field.values['items'] = [ ('42', '42'), ('88', '88') ]
+        
+        items1 = list_field.render()
+        
+        list_field.tales['items'] = TALESMethod("python:[42, 88]")
+        items2 = list_field.render()
+        
+        self.assertEquals(items1, items2)
+
+        list_field.validate({'field_list_field':'42'})
+
 
     def test_labels(self):
         self.form.manage_addField(
