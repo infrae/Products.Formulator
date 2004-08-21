37e5d35ff54a0a74e8700303fcceec0bc172eb32
Infrae <info@infrae.com>
2004-08-21 21:28:14 +0000

    helper to convert results from utf-8 to unicode not really elegant put hopefully foolproof for the several cases of nested lists, etc

--- helpers.py
+++ helpers.py
@@ -4,3 +4,24 @@ seq_types = [type([]), type(())]
 
 def is_sequence(v):
     return type(aq_base(v)) in seq_types
+
+def convert_unicode(struct):
+    """ convert all strings of a possibly deeply nested structure
+    of lists from utf-8 to unicode
+    in case of a dictionary only converts the values, not the keys
+    """
+
+    if type(struct) == type(''):
+        return unicode(struct, 'utf-8')
+
+    if type(struct) == type([]):
+        return map( convert_unicode, struct )
+
+    if type(struct) == type(()):
+        return tuple( map( convert_unicode, struct ) )
+
+    if type(struct) == type({}):
+        new_dict = {}
+        for k,v in struct.items():
+            new_dict[k] = convert_unicode(v)
+        return new_dict
