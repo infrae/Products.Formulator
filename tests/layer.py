1e5bfe4b902e10f335543ac9b158f6c990c09689
Sylvain Viollon <thefunny@gmail.com>
2010-01-22 17:03:42 +0000

    Remove spaces.

--- tests/layer.py
+++ tests/layer.py
@@ -3,13 +3,13 @@ from zope.testing.cleanup import cleanUp as _cleanUp
 def setDebugMode(mode):
     import Products.Five.fiveconfigure as fc
     fc.debug_mode = mode
-    
+
 def cleanUp():
     """Clean up component architecture."""
     _cleanUp()
     import Products.Five.zcml as zcml
     zcml._initialized = 0
-    
+
 class FormulatorZCMLLayer:
     @classmethod
     def setUp(cls):
