02ff9d456aa8cbe26615894cf5359d5ec25a554e
Michael Howitz <mh@gocept.com>
2022-05-20 13:38:17 +0200

    Move code to src + lint and format it.

--- Products/Formulator/HelperFields.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (c) 2013  Infrae. All rights reserved.
-# See also LICENSE.txt
-# include some helper fields which are in their own files
-from Products.Formulator.MethodField import MethodField
-from Products.Formulator.ListTextAreaField import ListTextAreaField
-from Products.Formulator.TALESField import TALESField
-
-
